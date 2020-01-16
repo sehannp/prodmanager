6d0c13ab7e5260bde661feecabe8778ea3976a70
sehannp <sehanp@gmail.com>
2020-01-16 11:30:56 +1100

    completed mysql based solution

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-items');
 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
@@ -40,7 +42,11 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
+//{force:true}
 sequelize.sync()
 .then(result => {
     return User.findByPk(1);
