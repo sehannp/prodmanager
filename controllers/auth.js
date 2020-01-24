1ee257aff7b16de99eb4af867bac765a9f044821
sehannp <sehanp@gmail.com>
2020-01-24 16:41:52 +1100

    added validation to login page

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,24 +10,30 @@ const {validationResult} = require('express-validator/check');
 const User = require('../models/user');
 
 exports.getLogin = (req,res,next) => {
+  const {email,password} = "";
     res.render('auth/login', {
       path:"/login", 
       pageTitle: 'Login',
-      errorMessage: req.flash('error')[0]
+      errorMessage: req.flash('error')[0],
+      oldInput: {email,password},
+      validationErrors: []
     });
 }
 
 exports.getSignup = (req,res,next) => {
+  const {email, password, confirmPassword} = "";
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: req.flash('error')[0]
+    errorMessage: req.flash('error')[0],
+    oldInput: {email, password, confirmPassword},
+    validationErrors: []
   });
 };
 
 
 exports.postLogin = (req,res,next) => {  
-  const {email, password}= req.body;
+  let {email, password}= req.body;
   const errors = validationResult(req);
 
   if(!errors.isEmpty()){
@@ -35,7 +41,9 @@ exports.postLogin = (req,res,next) => {
       'auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: errors.array()[0].msg
+        errorMessage: errors.array()[0].msg,
+        oldInput: {email,password},
+        validationErrors: errors.array()
       });
   }
   const fetchedPassword = req.userval.password
@@ -49,8 +57,15 @@ exports.postLogin = (req,res,next) => {
           res.redirect('/');
         });
       }
-      req.flash('error','invalid email or password')
-      res.redirect('/login');
+
+      res.status(422).render(
+        'auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'invalid email or password',
+          oldInput: {email,password},
+          validationErrors: ["password","email"]
+        });
     })
     .catch(err => {
       console.log(err);
@@ -66,7 +81,7 @@ exports.postLogout = (req,res,next) => {
 };
 
 exports.postSignup = (req,res,next) => {
-  const {email, password} = req.body;
+  const {email, password, confirmPassword} = req.body;
   const errors = validationResult(req);
 
   if(!errors.isEmpty()){
@@ -75,7 +90,9 @@ exports.postSignup = (req,res,next) => {
       'auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: errors.array()[0].msg
+        errorMessage: errors.array()[0].msg,
+        oldInput: {email, password, confirmPassword},
+        validationErrors: errors.array()
       });
   }
   bcrypt.hash(password,12)
