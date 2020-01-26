92d19cfeb0d5e53ffb75f55b81571ce7edad293a
sehannp <sehanp@gmail.com>
2020-01-26 19:09:07 +1100

    Added payment facility with stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 
+const STRIPE_KEY = require('../env/stripe'); 
+const stripe = require('stripe')(STRIPE_KEY);
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -208,3 +211,76 @@ exports.getInvoice = (req,res,next) => {
   })
   .catch(err => next(err));
 }
+
+exports.getCheckout = (req,res,next) => {
+
+  let products;
+  let total = 0;
+
+  req.user.populate('cart.items.productId')
+  .execPopulate()
+  .then(user => {
+    total = 0;
+    products = user.cart.items;
+    products.forEach(product => {
+      total+= product.quantity * product.productId.price;
+    });
+
+    return stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: products.map(p => {
+        return {
+          name: p.productId.title,
+          description: p.productId.description,
+          amount: p.productId.price * 100,
+          currency: 'usd',
+          quantity: p.quantity
+        };
+      }),
+      success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+      cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+    });
+  })
+  .then( session =>  {
+    res.render('shop/checkout', {
+      path:"/shop/checkout", 
+      pageTitle: 'Checkout',
+      products: products,
+      totalSum: total,
+      sessionId: session.id
+    }); 
+  })
+  .catch(err => {
+    const error = new Error(err)
+    error.httpStatus = 500;
+    return next(error);
+   });
+  };
+
+  exports.getCheckoutSuccess = (req,res,next) => {
+    req.user.populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {quantity: i.quantity, product: {...i.productId._doc}};
+      });
+  
+      const order = new Order
+      ({
+        user:{email: req.user.email, userId: req.user._id},
+        products: products 
+      });
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      const error = new Error(err)
+      error.httpStatus = 500;
+      return next(error);
+    });
+  }
